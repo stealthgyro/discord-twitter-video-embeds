bdad639b62a9172c9afc477b93d98df410a700d6
Stealthgyro <Stealthgyro@gmail.com>
2024-01-12 17:25:03 +0000

    scratchpad file for reference

---
+++ Scratchpad.sh
@@ -0,0 +1,2 @@
+#Possibly jus some useful commands, or just me playing around with stuff, figured I'd keep it here for reference
+docker-compose down && docker-compose build && docker-compose up -d && docker logs -f dev-tilted-embed
