546047c35fca082e43cf64d0c1a28cf8352a052b
Stealthgyro <Stealthgyro@gmail.com>
2024-01-25 19:09:50 +0000

    scratchpad notes

--- Scratchpad.sh
+++ Scratchpad.sh
@@ -3,3 +3,6 @@ docker-compose down && docker-compose build && docker-compose up -d && docker lo
 
 #guilty puppet meme.
 docker-compose down && docker-compose build && docker-compose up -d && docker logs -f tilted-embed
+
+# Stack changed or something like that and docker compose down didn't work...
+docker stop tilted-embed && && docker rm tilted-embed && docker compose build && docker compose up -d && docker logs -f tilted-embed
